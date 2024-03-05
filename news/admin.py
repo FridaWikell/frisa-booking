f623a86373f19dca7bc43cebba0c4e1ed4b0186e
Frida Wikell <fridasvensson7@gmail.com>
2024-03-05 11:51:24 +0000

    Add: New NewsAdmin class to extend usability in admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import News
 
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'course', 'created_on')  # Fields to display in the list view
+    list_filter = ('created_on', 'course')  # Fields to filter by in the list view
+    date_hierarchy = 'created_on'
+    prepopulated_fields = {'slug': ('title',)}
+
 # Register your models here.
-admin.site.register(News)
