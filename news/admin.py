f95e07ff788ed790536992e4c3a314fa1884bd82
Frida Wikell <fridasvensson7@gmail.com>
2024-03-05 12:05:57 +0000

    Add: Logged in staff user can only write news as themselfs

--- news/admin.py
+++ news/admin.py
@@ -4,9 +4,20 @@ from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'course', 'created_on')  # Fields to display in the list view
-    list_filter = ('created_on', 'course')  # Fields to filter by in the list view
+    list_display = ('title', 'author', 'course', 'created_on')
+    list_filter = ('created_on', 'course')
     date_hierarchy = 'created_on'
     prepopulated_fields = {'slug': ('title',)}
+    
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Override to limit the 'author' field dropdown to only the logged-in user.
+        """
+        form = super(NewsAdmin, self).get_form(request, obj, **kwargs)
+        user = request.user
 
-# Register your models here.
+        if not user.is_superuser:
+            form.base_fields['author'].queryset = form.base_fields['author'].queryset.filter(id=user.id)
+        return form
+
+
