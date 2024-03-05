97710b5628309270f8b09e487dbabbc9e29410b9
Frida Wikell <fridasvensson7@gmail.com>
2024-03-05 08:08:16 +0000

    Fix: Add maxlength to contact form content field

--- about/forms.py
+++ about/forms.py
@@ -4,4 +4,4 @@ class ContactForm(forms.Form):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea, label='Message')
+    content = forms.CharField(widget=forms.Textarea, max_length=10000, label='Message')
