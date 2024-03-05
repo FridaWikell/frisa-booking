ef2826f6d3156d5a47849eef45f92b3126a0d2dd
Frida Wikell <fridasvensson7@gmail.com>
2024-03-05 09:33:42 +0000

    Add: String to show course name in admin panel

--- booking/models.py
+++ booking/models.py
@@ -6,6 +6,9 @@ class Course(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class CourseSession(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
