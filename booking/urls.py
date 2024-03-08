d0d3704e8bacebb45a67f752e8c305e9c8a2bbb4
Frida Wikell <fridasvensson7@gmail.com>
2024-03-08 13:09:34 +0000

    Fix: Too many characters and add blankline in url booking

--- booking/urls.py
+++ booking/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.list_courses, name="booking"),
-    path('book_session/<int:session_id>/', views.book_session, name='book_session'),
-    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
-    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('book_session/<int:session_id>/', views.book_session,
+         name='book_session'),
+    path('edit_booking/<int:booking_id>/', views.edit_booking,
+         name='edit_booking'),
+    path('delete_booking/<int:booking_id>/', views.delete_booking,
+         name='delete_booking'),
     path('my_bookings/', views.my_bookings, name='my_bookings'),
     path('success/', views.success_page, name='success_page'),
 ]
