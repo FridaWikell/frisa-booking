2b0da1ab03086ba5e6757d3db8f90da97a40eb17
Frida Wikell <fridasvensson7@gmail.com>
2024-03-08 14:12:36 +0000

    Add: Docstring to news list in news app

--- news/views.py
+++ news/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views import generic
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import News
 
 
 @staff_member_required
 def news_list(request):
+    """
+    Render all news if the logged in user is and is a staff member or superuser.
+    """
     news_items = News.objects.all()
     return render(request, 'news/news.html', {
         'news_items': news_items
