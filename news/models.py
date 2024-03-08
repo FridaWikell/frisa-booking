3cd58b36504abe680cd82e2bebd92222969e5d8b
Frida Wikell <fridasvensson7@gmail.com>
2024-03-08 14:08:16 +0000

    Fix: Add docstring and edit code in models according to pep8

--- news/models.py
+++ news/models.py
@@ -4,10 +4,15 @@ from booking.models import Course
 
 
 class News(models.Model):
+    """
+    Django database model for news
+    """
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=150, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_update")
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="news_course")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_update")
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="news_course")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
