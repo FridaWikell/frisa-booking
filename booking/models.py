23bf990b6f175a7306a04753f08e1a18ce2003b2
Frida Wikell <fridasvensson7@gmail.com>
2024-03-09 09:03:43 +0000

    Fix: Replace single quotes with double quotes for consistency

--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    '''Django database model for workshop courses'''
+    """
+    Django database model for workshop courses
+    """
     name = models.CharField(max_length=100)
     description = models.TextField()
 
@@ -12,7 +14,9 @@ class Course(models.Model):
 
 
 class CourseSession(models.Model):
-    '''Django database model for workshop sessions'''
+    """
+    Django database model for workshop sessions
+    """
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -24,7 +28,9 @@ class CourseSession(models.Model):
 
 
 class Booking(models.Model):
-    '''Django database model for book a workshop course'''
+    """
+    Django database model for book a workshop course
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course_session = models.ForeignKey(CourseSession, on_delete=models.CASCADE)
     booking_date = models.DateTimeField(auto_now_add=True)
