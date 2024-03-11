12bd0589258551b809e005279d1e169bb79fa806
Frida Wikell <fridasvensson7@gmail.com>
2024-03-11 11:09:28 +0000

    Delete: Remove view and url to 500 page after manual testing

--- frisa/urls.py
+++ frisa/urls.py
@@ -28,14 +28,6 @@ def homepage_view(request):
     return HttpResponse(template.render({}, request))
 
 
-def fivehoundred_view(request):
-    """
-    Render the 500 page
-    """
-    template = loader.get_template('500.html')
-    return HttpResponse(template.render({}, request))
-
-
 urlpatterns = [
     path('about/', include('about.urls')),
     path('accounts/', include('allauth.urls')),
@@ -44,5 +36,4 @@ urlpatterns = [
     path('news/', include('news.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', homepage_view, name='home'),
-    path('500/', fivehoundred_view, name='500')
 ]
