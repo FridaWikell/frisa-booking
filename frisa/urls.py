bfdfbb4dd940a2c3cdaad34d20a38162bbdcf147
Frida Wikell <fridasvensson7@gmail.com>
2024-03-11 13:27:07 +0000

    Add: 500 url and views for html valid in deployed

--- frisa/urls.py
+++ frisa/urls.py
@@ -28,6 +28,15 @@ def homepage_view(request):
     return HttpResponse(template.render({}, request))
 
 
+
+def this_view(request):
+    """
+    Render the index page
+    """
+    template = loader.get_template('500.html')
+    return HttpResponse(template.render({}, request))
+
+
 urlpatterns = [
     path('about/', include('about.urls')),
     path('accounts/', include('allauth.urls')),
@@ -36,4 +45,5 @@ urlpatterns = [
     path('news/', include('news.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', homepage_view, name='home'),
+    path('500/', this_view, name='500'),
 ]
