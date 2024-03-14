c9f681c20f3aad8782645d9a021d4cae32fd919c
Frida Wikell <fridasvensson7@gmail.com>
2024-03-14 08:08:35 +0000

    Delete: Remove not used staticfiles

--- staticfiles/js/delete_booking.js
+++
@@ -1,8 +0,0 @@
-/* Submits the deletion form when user has confirmed it in the modal */
-document.querySelectorAll('.delete-booking').forEach(button => {
-    button.addEventListener('click', function() {
-        let bookingId = this.getAttribute('data-booking-id');
-        let form = document.getElementById('deleteForm');
-        form.action = `/booking/delete_booking/${bookingId}/`;
-    });
-});
