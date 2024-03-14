0e46756c4aa2c40594b664e2bf58f3e27c29dee1
Frida Wikell <fridasvensson7@gmail.com>
2024-03-14 07:59:08 +0000

    Add: Set debug to false before deployment

--- frisa/settings.py
+++ frisa/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fridawikell-frisabookin-1z0vled9q65.ws-eu108.gitpod.io',
                 '8000-fridawikell-frisabookin-8y4e7irxokf.ws-eu108.gitpod.io',
